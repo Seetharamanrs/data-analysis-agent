eca46bf164b3cbda11c8847c9ce9887189043cf9
seetharamanrs <seetharaman2632001@gmail.com>
2026-08-30 18:32:37 +0530

    The Query is added to check the database

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from huggingface_hub import InferenceClient
+from database import query_database
 from dotenv import load_dotenv
 import os
 
@@ -24,8 +25,26 @@ if you aren't sure of something reply that's i am out of content """
         "content": "What is the average of 10, 20, 30, 40 and 50?"
     }
 ]
+
 response = client.chat.completions.create(
     model=model,
     messages=messages
 )
 print(response.choices[0].message.content)
+query = """
+SELECT
+    c.city,
+    SUM(oi.quantity * oi.unit_price) AS total_sales
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+WHERE o.order_status = 'Completed'
+GROUP BY c.city
+ORDER BY total_sales DESC;
+"""
+
+result = query_database(query)
+
+print(result)
